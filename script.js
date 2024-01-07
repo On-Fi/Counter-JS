1109f449ed4a3843498057694fd092936fa5e701
onur fistikci <onurfistikci@mail.de>
2024-01-07 19:53:30 +0100

    make the app work

---
+++ script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const counter = document.getElementById('counter');
+    const increaseButton = document.getElementById('increase');
+    const resetButton = document.getElementById('reset');
+    const decreaseButton = document.getElementById('decrease');
+
+    let count = 0;
+
+    increaseButton.addEventListener('click', function () {
+        count++;
+        updateCounter();
+    });
+
+    resetButton.addEventListener('click', function () {
+        count = 0;
+        updateCounter();
+    });
+
+    decreaseButton.addEventListener('click', function () {
+        count--;
+        updateCounter();
+    });
+
+    function updateCounter() {
+        counter.textContent = count;
+    }
+});
